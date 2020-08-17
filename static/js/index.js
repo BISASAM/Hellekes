89011890b8433809245e135c7179920c4c5f179c
Vinc <39947530+BISASAM@users.noreply.github.com>
2020-08-17 12:41:28 +0200

    vorbereitungen zum filtern
    siehe "fill_table()" und "get_attr_to_mark()" funktionen

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,11 @@ const goBtn = document.getElementById("goBtn");
 const resultTable = document.getElementById("resultTable");
 const fileInput = document.getElementById("fileInput");
 const colSelectDiv = document.getElementById("col_select");
+
+const inp_minBetrag = document.getElementById("minBetrag");
+const inp_minAnzahl = document.getElementById("minAnzahl");
+const inp_varianz = document.getElementById("varianz");
+const inp_minKorrIdx = document.getElementById("minKorrIdx");
 var jsonData;
 
 
@@ -37,10 +42,15 @@ function fill_table(data) {
         row.innerHTML = `<th scope="row">${i++}</th>
                          <td>TBD</td>`
 
-        for (attr in transaction) {
+        // fill cells
+        cells_to_mark = get_attr_to_mark(transaction);
+        for (attr in transaction) { 
             let td = document.createElement('td');
-            td.classList.add(attr);
+            td.classList.add(attr);  //needed to hide/unhide column
             td.innerHTML = transaction[attr];
+            if (cells_to_mark.has(attr)) {
+                // attached class to row in order to mark it
+            } 
             row.appendChild(td);
         }
         
@@ -94,6 +104,13 @@ function toggleCol(event) {
     }
     console.log(elements_to_hide);
 
+}
+
+function get_attr_to_mark(transaction) {
+    "This function takes a transaction as input, applies filter and returns a set, that defines which table cellls to mark"
+    "output example: {iban, purpose, amount}"
+
+    return new Set([]);
 }
 
 
