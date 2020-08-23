9b17c8c26707feb53af05134f29adb930ca4554d
BISASAM <39947530+BISASAM@users.noreply.github.com>
2020-08-23 14:39:17 +0200

    added page to edit signal words

--- main.py
+++ main.py
@@ -13,11 +13,62 @@ def cpi():
     return render_template("corr_idx.html")
 
 
+@app.route('/sig')
+def sig():
+    return render_template("sig_wrds.html")
+
+
 @app.route('/get_cpi_scores')
 def read_cpi_json():
     with open('data/cpi_scores.json') as f:
         data = json.load(f)
     return jsonify(data)
+
+
+@app.route('/get_signal_words')
+def read_sig_json():
+    with open('data/signal_words.json', encoding='utf8') as f:
+        data = json.load(f)
+        data = [word.lower() for word in data]
+    return jsonify(data)
+
+
+@app.route('/new_sig_word', methods=['GET'])
+def new_sig_word():
+    new_word = request.args.get("n")
+
+    word_list = get_signal_words()
+    word_list.add(new_word)
+
+    write_signal_words(word_list)
+
+    return make_response('Ok', 200)
+
+
+@app.route('/change_sig_word', methods=['GET'])
+def change_sig_word():
+    old_word = request.args.get("o")
+    new_word = request.args.get("n")
+
+    word_list = get_signal_words()
+    word_list.remove(old_word)
+    word_list.add(new_word)
+
+    write_signal_words(word_list)
+
+    return make_response('Ok', 200)
+
+
+@app.route('/del_sig_word', methods=['GET'])
+def del_sig_word():
+    word = request.args.get("w")
+
+    word_list = get_signal_words()
+    word_list.remove(word)
+
+    write_signal_words(word_list)
+
+    return make_response('Ok', 200)
 
 
 @app.route('/get_data')
@@ -43,6 +94,28 @@ def change_cpi_score():
         json.dump(data, f)
 
     return make_response('Ok', 200)
+
+
+def get_signal_words():
+    """returns a Set"""
+    data = []
+    with open('data/signal_words.json', "r", encoding='utf8') as f:
+        data = json.load(f)
+    
+    data = [word.lower() for word in data]
+    data = set(data)
+
+    return data
+
+
+def write_signal_words(data):
+    """input should be a Set"""
+    data = list(data)
+    with open('data/signal_words.json', "w", encoding='utf8') as f:
+        json.dump(data, f)
+
+
+   
 
 
 if __name__ == '__main__':
---
+++ static/js/sig_wrds.js
@@ -0,0 +1,132 @@
+const resultTableBody = document.getElementById("resultTableBody");
+const contextMenu = document.getElementById("contextMenu");
+const $contextMenu = $("#contextMenu");
+
+//context menu buttons
+const newBtn = document.getElementById("newBtn");
+const editBtn = document.getElementById("editBtn");
+const delBtn = document.getElementById("delBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+
+let contextMenuVisible;
+let old_sig_word;
+let signal_words;
+let mode;
+
+document.onload = initialze();
+
+async function initialze() {
+  countryCodeOfRow = -1;
+
+  signal_words = await get_signal_words();
+  fill_table(signal_words);
+
+  // on click hide context menu
+  window.addEventListener("click", function () {
+    if (contextMenuVisible) {
+      $contextMenu.hide();
+      contextMenuVisible = false;
+    }
+  });
+
+  //context menu buttons
+  editBtn.addEventListener("click", onEditBtn);
+  newBtn.addEventListener("click", onNewtBtn);
+  delBtn.addEventListener("click", onDeltBtn);
+  editModalBtn.addEventListener("click", onSaveBtn);
+  //delBtn.addEventListener('click', onDelBtn);
+  cancelBtn.addEventListener("click", function () {
+    countryCodeOfRow = -1;
+  });
+}
+
+async function get_signal_words() {
+  const response = await fetch("/get_signal_words");
+  // 200 means "ok".
+  if (response.status !== 200) {
+    console.log("Problem with api: " + response.status);
+  }
+
+  const sig_word = await response.json();
+  return sig_word;
+}
+
+function fill_table(data) {
+  resultTableBody.innerHTML = ""; // clear table
+
+  // create table body
+  i = 1;
+  for (const word of data) {
+    let row = document.createElement("tr");
+    row.innerHTML = `<th scope="row">${i++}</th>
+                    <td>${word}</td>`;
+    row.addEventListener("contextmenu", showContextMenu);
+    resultTableBody.appendChild(row);
+  }
+}
+
+function showContextMenu(event) {
+  event.preventDefault();
+  $contextMenu.css({
+    display: "block",
+    left: event.pageX,
+    top: event.pageY,
+  });
+  contextMenuVisible = true;
+  old_sig_word = event.target.parentElement.getElementsByTagName("td")[0]
+    .innerHTML;
+
+  return false;
+}
+
+function onEditBtn() {
+  mode = "edit";
+  showEditModal(old_sig_word, "edit");
+}
+
+function onNewtBtn() {
+  mode = "new";
+  showEditModal("Neues Wort", "new");
+}
+
+async function onDeltBtn() {
+  const response = await fetch(`del_sig_word?w=${old_sig_word}`);
+  if (response.status !== 200) {
+    console.log("Problem with api: " + response.status);
+  } else {
+    old_sig_word = -1;
+    initialze();
+    console.log("supi");
+  }
+}
+
+function showEditModal(data) {
+  const editModalBody = document.getElementById("editModalBody");
+
+  //clear table
+  editModalBody.innerHTML = "";
+
+  editModalBody.innerHTML = `
+    Signalwort: <input type="text" id="sig_word" value="${data}"> <br>
+    `;
+  $("#editModal").modal();
+}
+
+async function onSaveBtn() {
+  let new_sig_word = document.getElementById("sig_word").value;
+
+  let url =
+    mode == "new"
+      ? `new_sig_word?n=${new_sig_word}`
+      : `change_sig_word?o=${old_sig_word}&n=${new_sig_word}`;
+
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    console.log("Problem with api: " + response.status);
+  } else {
+    old_sig_word = -1;
+    initialze();
+    console.log("supi");
+  }
+  $("#editModal").modal("hide");
+}
